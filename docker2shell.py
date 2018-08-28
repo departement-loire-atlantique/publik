751bea8bb9f6b51d664ce29eef2694e826c94abd
Julien BAYLE <julien@021347C-GRU.cg44.fr>
2018-08-28 17:53:03 +0200

    Add script for shell install

---
+++ docker2shell.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+
+import os
+import re
+import shutil
+import glob
+
+# This script converts from the docker version to a shell install version
+# Usefull for installation without docker
+
+# Manual installation
+# - Init tout database with postgresql/docker-entrypoint-initdb.d/init-database.sh
+# - Add db, smtp and rabbitmq to /etc/hosts
+# - Excute ./docker2shell.py
+# - Open schellinstall folder
+#   - Update database connexion in config files (*.py)
+#   - Create env file and load data in bash session for envsub
+#   - Update NGINX ports in *.template
+#   - Update SMTP config (config.py and wcs.settings.py)
+#   - Turn debug off if needed in config files (*.py)
+#   - Execute install.sh
+#   - Start app with start-xxx.sh
+
+def file_replace(replace_dict, source, dest=None, title=None):
+    fin = open(source, 'r')
+    if dest:
+        fout = open(dest, 'a')
+    else:
+        fd, name = mkstemp()
+        fout = open(name, 'w')
+
+    if title:
+        fout.write("\n\n################\n")
+        fout.write("# " + title + "\n")
+        fout.write("################\n\n")
+
+    for line in fin:
+        out = line
+        for pattern,replace in replace_dict.iteritems():
+            out = re.sub(pattern, replace, out)
+        fout.write(out)
+    try:
+        fout.writelines(fin.readlines())
+    except Exception as E:
+        raise E
+
+    fin.close()
+    fout.close()
+
+    if not dest:
+        shutil.move(name, source) 
+    
+apps = ["base", "hobo", "authentic", "combo", "fargo", "passerelle", "wcs"]
+
+project_path = os.getcwd()
+bare_path = os.path.join(project_path, "shellinstall")
+if not os.path.exists(bare_path):
+    os.makedirs(bare_path)
+else:
+    [os.remove(f) for f in glob.glob(bare_path+"/*")]
+
+replace_dict = {
+    "FROM" : "# FROM",
+    "MAINTAINER" : "# MAINTAINER",
+    "VOLUME" : "# VOLUME",
+    "EXPOSE" : "# EXPOSE",
+    "ENTRYPOINT" : "# ENTRYPOINT",
+    "CMD" : "# CMD",
+    "RUN " : "",
+    "COPY" : "cp",
+    "ENV\s(?P<name>[A-Z]*)\s*(?P<value>[a-z]*)" : "export \g<name>=\g<value>",
+    "/root" : "/usr/bin"
+    }
+
+do_not_copy = ["Dockerfile", "LICENSE", "README.md", \
+    ".git", "nginx.template", "docker-entrypoint.sh"]
+
+for app in apps:
+    app_path = os.path.join(project_path, app)
+    
+    if "Dockerfile" in os.listdir(app) :
+        print("Converting {} docker image...".format(app))
+
+        # Rename nginx template using app name
+        nginx_path = os.path.join(app_path, "nginx.template")
+        if os.path.isfile(nginx_path):
+            nginx_new_name = "nginx-"+app+".template"
+            replace_dict.update({"nginx.template" : nginx_new_name})
+            shutil.copyfile(nginx_path, os.path.join(bare_path, nginx_new_name))
+        
+        # Convert docker-entrypoint into startup script
+        entrypoint_path = os.path.join(app_path, "docker-entrypoint.sh")
+        if os.path.isfile(entrypoint_path):
+            startappscript = "start-"+app+".sh"
+            replace_dict.update({"docker-entrypoint.sh" : startappscript})
+            shutil.copyfile(entrypoint_path, os.path.join(bare_path, startappscript))
+
+        # Convert dockerfile
+        file_replace(replace_dict, \
+            os.path.join(app_path, "Dockerfile"), \
+            os.path.join(bare_path, "install.sh"), app)
+        
+        # Copy other files
+        files = [f for f in os.listdir(app) if f not in do_not_copy]
+        for file in files:
+            file_path = os.path.join(app_path, file)
+            if os.path.isfile(os.path.join(bare_path, file)):
+                print("Error, file %s already exists", file)
+            shutil.copy(file_path, bare_path)
+        print("{} docker image converted".format(app))
