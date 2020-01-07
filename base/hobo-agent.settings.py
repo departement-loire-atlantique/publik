7a69552596351c1b71c395a222cbba6d0ec5b131
Vincent Lefoulon <vincent.lefoulon@free.fr>
2020-01-07 15:28:16 +0100

    Add env vars to configure base

--- base/hobo-agent.settings.py
+++ base/hobo-agent.settings.py
@@ -1,4 +1,8 @@
 # AMQP message broker
 # http://celery.readthedocs.org/en/latest/configuration.html#broker-url
 # transport://userid:password@hostname:port/virtual_host
-BROKER_URL = 'amqp://hobo:hobopass@rabbitmq:5672//'
+BROKER_URL = 'amqp://{user}:{password}@rabbitmq:{port}//'.format(
+    user=os.environ['RABBITMQ_USER'],
+    password=os.environ['RABBITMQ_PASS'],
+    port=os.environ['RABBITMQ_PORT'],
+)
