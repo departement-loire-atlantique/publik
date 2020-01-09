0315e62257988a9a4b703bb4bd73c6e0b3c0532b
Vincent Lefoulon <vincent.lefoulon@free.fr>
2020-01-09 17:37:53 +0100

    Fix typo

--- hobo/start.sh
+++ hobo/start.sh
@@ -6,7 +6,7 @@ set -eu
 # Wait for dependencies
 /root/wait-for-it.sh -t 60 db:${DB_PORT}
 /root/wait-for-it.sh -t 60 rabbitmq:${RABBITMQ_PORT}
-/root/subst_env.sh
+/root/subst-env.sh
 
 # Adapt configuration from ENV variables
 envsubst '${ENV} ${DOMAIN}' < /etc/nginx/conf.d/hobo.template > /etc/nginx/conf.d/hobo.conf
