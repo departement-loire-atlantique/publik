47ec576d51a9c52d0d04f06b1ecb021e1759668b
Vincent Lefoulon <vincent.lefoulon@free.fr>
2020-01-15 10:24:00 +0100

    Fix home dir when user already exists

--- install-on-debian.sh
+++ install-on-debian.sh
@@ -51,8 +51,8 @@ echo "Create publik user"
 
 if getent passwd publik > /dev/null 2>&1; then
     echo "User publik already exists"
-    mkdir -p /home/publik
     usermod publik -d /home/publik
+    usermod publik -s /bin/bash
 else
     useradd publik -m
     usermod publik -s /bin/bash
