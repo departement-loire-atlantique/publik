f43cdf4f41361d4d9156d287d4befe6ba169f190
Vincent Lefoulon <vincent.lefoulon@free.fr>
2020-01-20 18:02:05 +0100

    Add env vars for subdomains

--- base/subst-env.sh
+++ base/subst-env.sh
@@ -4,7 +4,7 @@ set -eu
 
 /root/check-env.sh
 
-envsubst '${ENV} ${DOMAIN}' < "/etc/nginx/conf.d/$1.template" > "/etc/nginx/conf.d/$1.conf"
+envsubst '${ENV} ${DOMAIN} $AUTHENTIC_SUBDOMAIN $COMBO_SUBDOMAIN $COMBO_ADMIN_SUBDOMAIN $FARGO_SUBDOMAIN $HOBO_SUBDOMAIN $PASSERELLE_SUBDOMAIN $WCS_SUBDOMAIN' < "/etc/nginx/conf.d/$1.template" > "/etc/nginx/conf.d/$1.conf"
 envsubst '$RABBITMQ_DEFAULT_USER $RABBITMQ_DEFAULT_PASS $RABBITMQ_PORT' < /etc/hobo-agent/settings.d/broker.template > /etc/hobo-agent/settings.d/broker.py
 envsubst '$DEBUG $ALLOWED_HOSTS $DB_PORT $DB_HOBO_PASS $DB_PASSERELLE_PASS $DB_COMBO_PASS $DB_FARGO_PASS $DB_WCS_PASS $DB_AUTHENTIC_PASS $RABBITMQ_DEFAULT_USER $RABBITMQ_DEFAULT_PASS $RABBITMQ_PORT $ERROR_MAIL_AUTHOR $ERROR_MAIL_ADDR $MAILCATCHER_SMTP_PORT' < /root/pyenv.template > /home/pyenv.py
 # Needs to be in a directory that can be read by a non-root user, so not "/root"
--- hobo/start.sh
+++ hobo/start.sh
@@ -8,9 +8,9 @@ set -eu
 /root/wait-for-it.sh -t 60 rabbitmq:${RABBITMQ_PORT}
 /root/subst-env.sh "hobo"
 
-envsubst '${EMAIL} ${ENV} ${DOMAIN} ${HTTPS_PORT} ${DB_WCS_PASS}' < /tmp/config.template > /tmp/config.json
-envsubst '${EMAIL} ${ENV} ${DOMAIN} ${HTTPS_PORT}' < /tmp/cook.sh.template > /tmp/cook.sh
-envsubst '${EMAIL} ${ENV} ${DOMAIN} ${HTTPS_PORT} ${SUPERUSER_PASS}' < /tmp/hobo.recipe.template > /tmp/recipe.json
+envsubst '${EMAIL} ${ENV} ${DOMAIN} ${HTTPS_PORT} ${DB_WCS_PASS} $COMBO_SUBDOMAIN' < /tmp/config.template > /tmp/config.json
+envsubst '${EMAIL} ${ENV} ${DOMAIN} ${HTTPS_PORT} $AUTHENTIC_SUBDOMAIN $COMBO_SUBDOMAIN $COMBO_ADMIN_SUBDOMAIN $FARGO_SUBDOMAIN $HOBO_SUBDOMAIN $PASSERELLE_SUBDOMAIN $WCS_SUBDOMAIN' < /tmp/cook.sh.template > /tmp/cook.sh
+envsubst '${EMAIL} ${ENV} ${DOMAIN} ${HTTPS_PORT} ${SUPERUSER_PASS} $AUTHENTIC_SUBDOMAIN $COMBO_SUBDOMAIN $COMBO_ADMIN_SUBDOMAIN $FARGO_SUBDOMAIN $HOBO_SUBDOMAIN $PASSERELLE_SUBDOMAIN $WCS_SUBDOMAIN' < /tmp/hobo.recipe.template > /tmp/recipe.json
 chmod +x /tmp/cook.sh
 
 # Start NGINX
