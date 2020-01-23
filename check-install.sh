302cdaed4d037b79b83d5cd35987fbcb8172b0e9
Vincent Lefoulon <vincent.lefoulon@free.fr>
2020-01-23 17:40:11 +0100

    Make sure folder is not empty

--- check-install.sh
+++ check-install.sh
@@ -1,12 +1,25 @@
 #!/bin/sh
 
-set -eu
+function check() {
+  container=$1
+  folder=$2
+  docker exec $container ls "$folder"  # Make sure folder exists
+  if [ $? -ne 0 ]; then
+    echo "ERROR: $folder on $container doesn't exist but should"
+    exit 1
+  fi
+  c=`docker exec $container ls "$folder" | wc -l`
+  if [ "$c" -eq "0" ]; then 
+    echo "ERROR: $folder on $container is empty but shouldn't"
+    exit 1
+  fi
+}
 
-docker exec authentic ls "/var/lib/authentic2-multitenant/tenants"
-docker exec combo ls "/var/lib/combo/tenants"
-docker exec fargo ls "/var/lib/fargo/tenants"
-docker exec hobo ls "/var/lib/hobo/tenants"
-docker exec passerelle ls "/var/lib/passerelle/tenants"
-docker exec wcs ls "/var/lib/wcs"
+check authentic "/var/lib/authentic2-multitenant/tenants"
+check combo "/var/lib/combo/tenants"
+check fargo "/var/lib/fargo/tenants"
+check hobo "/var/lib/hobo/tenants"
+check passerelle "/var/lib/passerelle/tenants"
+check wcs "/var/lib/wcs"
 
-echo "Installation OK"
+echo "Deployment looks OK"
